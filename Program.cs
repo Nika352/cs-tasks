cbfc07d932c7e71b4f590b05ff8e6e0e2b98b3e7
Nika352 <102316119+Nika352@users.noreply.github.com>
2023-02-26 01:29:49 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("enter int below this text");
+int enteredInt = int.Parse(Console.ReadLine());
+Boolean isEven = false;
+if (enteredInt % 2 == 0)
+{
+    isEven = true;
+}
+
+if (isEven)
+{
+    Console.WriteLine("entered integer is even");
+}
+else
+{
+    Console.WriteLine("entered integer is odd");
+}
